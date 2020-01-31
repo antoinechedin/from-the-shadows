4abf2590f99c7c771e048eb12d838a9639542557
Leomani <xboxlive84320@hotmail.fr>
2020-01-31 14:20:02 +0100

    Meta : mise en place de la population des champs

---
+++ Assets/Scripts/Menus/MenuMetaManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuMetaManager : MonoBehaviour
+{
+    public GameObject lightGuyValues;
+    public GameObject shadowGuyValues;
+    public GameObject totalValues;
+
+    private List<TextMeshProUGUI> lightGuyToPopulate;
+    private List<TextMeshProUGUI> shadowGuyToPopulate;
+    private List<TextMeshProUGUI> totalToPopulate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.LoadSaveFile(0);
+
+        lightGuyToPopulate = new List<TextMeshProUGUI>();
+        shadowGuyToPopulate = new List<TextMeshProUGUI>();
+        totalToPopulate = new List<TextMeshProUGUI>();
+
+        for (int i = 0; i < lightGuyValues.transform.childCount; i++)
+        {
+            lightGuyToPopulate.Add(lightGuyValues.transform.GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>());
+        }
+
+        for (int i = 0; i < shadowGuyValues.transform.childCount; i++)
+        {
+            shadowGuyToPopulate.Add(shadowGuyValues.transform.GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>());
+        }
+
+        for (int i = 0; i < totalValues.transform.childCount; i++)
+        {
+            totalToPopulate.Add(totalValues.transform.GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Populate();
+        }
+    }
+
+    public void Populate()
+    {
+        lightGuyToPopulate[0].text = GameManager.Instance.GetMetaInt("playerDeath1").ToString();
+        lightGuyToPopulate[1].text = GameManager.Instance.GetMetaInt("jumpNumber1").ToString();
+        lightGuyToPopulate[2].text = GameManager.Instance.GetMetaFloat("distance1").ToString();
+
+        shadowGuyToPopulate[0].text = GameManager.Instance.GetMetaInt("playerDeath2").ToString();
+        shadowGuyToPopulate[1].text = GameManager.Instance.GetMetaInt("jumpNumber2").ToString();
+        shadowGuyToPopulate[2].text = GameManager.Instance.GetMetaFloat("distance2").ToString();
+    }
+}
