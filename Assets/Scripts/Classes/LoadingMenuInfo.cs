37da2878818467778757c605a28f23de338a3c44
Leomani <xboxlive84320@hotmail.fr>
2020-02-05 11:27:32 +0100

    GameManager : structure de chargement du menu

---
+++ Assets/Scripts/Classes/LoadingMenuInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingMenuInfo
+{
+    private int startingMenuScene { get;} // Menu to open when going in the mainManu scene (mainMenu, SaveFile, chapterSelection)
+    private int startingChapterIndex { get; } // index of chapter position for the cursor in the chapterSelection
+
+    public LoadingMenuInfo(int startMenuScene)
+    {
+        startingMenuScene = startMenuScene;
+        startingChapterIndex = -1;
+    }
+
+    public LoadingMenuInfo(int startMenuScene, int startingChapter)
+    {
+        startingMenuScene = startMenuScene;
+        startingChapterIndex = startingChapter;
+    }
+}
