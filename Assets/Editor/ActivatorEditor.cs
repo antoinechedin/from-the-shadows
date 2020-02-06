a30c66f69ca6f2da295d8b5efc4417feda04548a
antoinechedin <antoine.chedin@gmail.com>
2020-02-06 15:05:48 +0100

    Add custom inspector for activators/listeners

---
+++ Assets/Editor/ActivatorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Activator), true)]
+public class ActivatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        Activator activator = target as Activator;
+
+        if(GUILayout.Button("Update Activators references"))
+        {
+           activator.UpdateListenerReferences();
+        }
+    }
+}
