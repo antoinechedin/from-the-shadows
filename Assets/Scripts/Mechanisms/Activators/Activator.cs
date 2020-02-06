7c8a8c2bd731df8fb0238c6fcc73ed901c04adc9
antoinechedin <antoine.chedin@gmail.com>
2020-02-06 14:58:57 +0100

    Add Activator and Listeners

---
+++ Assets/Scripts/Mechanisms/Activators/Activator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public List<ActivatorListener> listeners = new List<ActivatorListener>();
+
+    public delegate void OnActivate();
+    public OnActivate Activate;
+
+    public delegate void OnDeactivate();
+    public OnDeactivate Deactivate;
+
+    public void UpdateListenerReferences()
+    {
+        ActivatorListener[] allListeners = GameObject.FindObjectsOfType<ActivatorListener>();
+        for (int i = 0; i < allListeners.Length; i++)
+        {
+            if (allListeners[i].activators.Contains(this) && !listeners.Contains(allListeners[i]))
+            {
+                allListeners[i].activators.Remove(this);
+            }
+
+            if (listeners.Contains(allListeners[i]) && !allListeners[i].activators.Contains(this))
+            {
+                allListeners[i].activators.Add(this);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanisms/Listeners/ActivatorListener.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActivatorListener : MonoBehaviour
+{
+    public List<Activator> activators = new List<Activator>();
+    
+    public abstract void OnActivate();
+
+    public abstract void OnDeactivate();
+
+    /// <summary>
+    /// Used by the ActivatorEditor class to update Activator/Activable references.static
+    /// Do not call this method outside of the Editor.
+    /// </summary>
+    public void UpdateActivatorReferences()
+    {
+        Activator[] allActivators = GameObject.FindObjectsOfType<Activator>();
+        for (int i = 0; i < allActivators.Length; i++)
+        {
+            if (allActivators[i].listeners.Contains(this) && !activators.Contains(allActivators[i]))
+            {
+                allActivators[i].listeners.Remove(this);
+            }
+
+            if (activators.Contains(allActivators[i]) && !allActivators[i].listeners.Contains(this))
+            {
+                allActivators[i].listeners.Add(this);
+            }
+        }
+    }
+}
