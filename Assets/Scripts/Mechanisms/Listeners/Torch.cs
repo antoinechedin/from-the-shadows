770be6184ef9d867f33f89b5aaf84d9d50edc712
GaelleMarais <marais.gaelle@gmail.com>
2020-02-07 15:41:13 +0100

    Add torch and edit existing prefab

---
+++ Assets/Scripts/Mechanisms/Listeners/Torch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : ActivatorListener
+{
+    private bool active;
+    public GameObject lightSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lightSource = transform.Find("LightSource").gameObject;
+    }
+
+    public override void OnActivate()
+    {
+        active = true;
+        lightSource.SetActive(active);
+    }
+
+    public override void OnDeactivate()
+    {
+        active = false;
+        lightSource.SetActive(active);
+    }
+}
