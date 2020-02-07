264522e95dd1b662487086999921fe2adfc5acda
GaelleMarais <marais.gaelle@gmail.com>
2020-02-07 12:01:10 +0100

    Add prefabs and gizmos for mechanisms

--- Assets/Scripts/Mechanisms/Activators/Lever.cs
+++ Assets/Scripts/Mechanisms/Activators/Lever.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Lever : Activator
 {
     bool active;
     private void Start()
-    { 
+    {
         if (Deactivate != null)
             Deactivate();
 
         active = false;
     }
+
     public void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && Input.GetButtonDown("X_G"))
---
+++ Assets/Scripts/Mechanisms/Listeners/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class Door : ActivatorListener
+{
+    public bool open;
+
+    private void Start()
+    {
+        GetComponent<BoxCollider2D>().enabled = open;
+    }
+
+    public override void OnActivate()
+    {
+        open = !open;
+        GetComponent<BoxCollider2D>().enabled = open;
+        GetComponent<MeshRenderer>().enabled = open;
+    }
+
+    public override void OnDeactivate()
+    {
+        open = !open;
+        GetComponent<BoxCollider2D>().enabled = open;
+        GetComponent<MeshRenderer>().enabled = open;
+    }
+}
