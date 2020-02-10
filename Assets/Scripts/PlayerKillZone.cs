531498192c86b05da303493f27868581ffef6004
Leomani <xboxlive84320@hotmail.fr>
2020-02-10 16:40:50 +0100

    Mort des joueurs : debut
    Pour le moment les joueurs meurent et sont tp à leur point de spawn et le meta est incémenté. Il faut implémenter un fond au noir.

---
+++ Assets/Scripts/PlayerKillZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKillZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            int pId = collision.GetComponent<PlayerController>().id;
+            Debug.Log("Le joueur " + pId + " est mort");
+            GameObject.FindObjectOfType<ChapterManager>().ResetLevel(pId);
+        }
+    }
+}
