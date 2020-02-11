4bd58265de099132663008d871273d291d9913b3
Seransei <marrocq.alois@laposte.net>
2020-02-11 16:14:37 +0100

    Volume buttons laid out, need to set up

--- Assets/Scripts/UI/Menus/MenuOptions.cs
+++ Assets/Scripts/UI/Menus/MenuOptions.cs
@@ -7,42 +7,42 @@ using System;
 
 public class MenuOptions : MonoBehaviour
 {
-    public Slider volumeSlider;
-    public Text resolutionText;
-    public Toggle toggleFullscreen;
+    public Selectable volumeGO;
 
     private void Awake()
     {
-        volumeSlider.value = AudioListener.volume;
-        resolutionText.text = Screen.currentResolution.width + "x" + Screen.currentResolution.height;
-        toggleFullscreen.isOn = Screen.fullScreen;
+        
     }
 
     public void OpenOptionsMenu()
     {
-        EventSystem.current.SetSelectedGameObject(volumeSlider.gameObject);
+        EventSystem.current.SetSelectedGameObject(volumeGO.gameObject);
     }
 
     private void Update()
     {
         if (Input.GetButtonDown("B_G"))
         {
-            Back();
+            Return();
         }
-    }
-
-    private void Back()
-    {
-        throw new NotImplementedException();
     }
 
     public void ChangeVolume()
     {
-        AudioListener.volume = volumeSlider.value;
+        //AudioListener.volume = ;
+    }
+
+    public void ChangeResolution()
+    {
+        //Screen.SetResolution();
     }
 
     public void ToggleFullscreen()
     {
         Screen.fullScreen = !Screen.fullScreen;
+    }
+    public void Return()
+    {
+        throw new NotImplementedException();
     }
 }
---
+++ Assets/Scripts/UI/OptionsArrowButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OptionsArrowButtons : MonoBehaviour, ISelectHandler
+{
+    public Selectable origin;
+    public float value;
+
+    bool done;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        done = false;
+        StartCoroutine(ChangeVolume());
+        done = true;
+    }
+
+    private IEnumerator ChangeVolume()
+    {
+        while (!done) 
+        {
+            yield return null;
+        }
+        AudioListener.volume += value;
+        EventSystem.current.SetSelectedGameObject(origin.gameObject);        
+    }
+}
