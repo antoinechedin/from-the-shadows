aac391e4fbf61aaee038457bc933239e62873529
antoinechedin <antoine.chedin@gmail.com>
2020-02-14 12:12:50 +0100

    Add player debug UI

---
+++ Assets/Scripts/Debug/PlayerDebugPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerDebugPanel : MonoBehaviour
+{
+    private TextMeshProUGUI playerInfos;
+    private string playerInfosTemplate;
+
+    private NewPlayerController player;
+
+    private void Awake()
+    {
+        // playerInfos = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+        playerInfos = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        if (playerInfos != null) playerInfosTemplate = playerInfos.text;
+        
+
+        player = transform.parent.parent.GetComponent<NewPlayerController>();
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            if (playerInfos != null)
+            {
+                playerInfos.text = string.Format(
+                    playerInfosTemplate,
+                    player.velocity.x,
+                    player.velocity.y,
+                    player.targetVelocity.x,
+                    player.targetVelocity.y,
+                    player.grounded ? "X" : "_",
+                    player.state
+                );
+            }
+        }
+    }
+}
