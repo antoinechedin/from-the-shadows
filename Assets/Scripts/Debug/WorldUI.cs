13db78e05898174b65e839e8bce79724eda16933
Seransei <marrocq.alois@laposte.net>
2020-02-17 16:44:49 +0100

    GUI element follows player
    need to make it general and create Panel

---
+++ Assets/Scripts/Debug/WorldUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldUI : MonoBehaviour
+{
+    public Transform player;
+
+    Image followImage;
+    public Sprite displayedSprite;
+
+    public Vector3 offSet;
+
+    public bool drawDebugPosition;
+
+    private void Start()
+    {
+        followImage = gameObject.AddComponent<Image>();
+        followImage.sprite = displayedSprite;
+        followImage.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+    private void Update()
+    {
+        followImage.rectTransform.position = Camera.main.WorldToScreenPoint(player.position + offSet);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(drawDebugPosition)
+            Gizmos.DrawWireSphere(player.position + offSet, 1);
+    }
+}
