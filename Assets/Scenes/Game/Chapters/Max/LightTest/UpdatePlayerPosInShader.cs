beebddbed92e6e48d80b5f1befd6e37018a773bd
Max Emsallem <emsallem.max@hotmail.fr>
2020-02-19 14:44:29 +0100

    Test with shaders

---
+++ Assets/Scenes/Game/Chapters/Max/LightTest/UpdatePlayerPosInShader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePlayerPosInShader : MonoBehaviour
+{    void Update()
+    {
+        Shader.SetGlobalVector("_PlayerPos", transform.position);
+    }
+}
