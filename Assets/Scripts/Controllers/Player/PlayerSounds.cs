109a842bde576466cd8193f8ed9bafba4eb6d961
antoinechedin <antoine.chedin@gmail.com>
2020-02-21 18:25:21 +0100

    Update sounds

---
+++ Assets/Scripts/Controllers/Player/PlayerSounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerSounds : MonoBehaviour
+{
+    public AudioClip[] footsteps;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandomFootstep()
+    {
+        if (footsteps.Length > 0)
+            audioSource.PlayOneShot(footsteps[Random.Range(0, footsteps.Length)]);
+    }
+}
