62296ea79863f49d965563d0842daec4790d1f6f
GaelleMarais <marais.gaelle@gmail.com>
2020-02-24 17:01:09 +0100

    Laser activators

---
+++ Assets/Scripts/Props/Activators/Laser.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private LayerMask collisionMask;
+    private Vector3[] points;
+    private Vector3 position;
+
+    public float range = 100;
+    public int maxReflection = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        points = new Vector3[maxReflection + 1];
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = maxReflection + 2;
+        collisionMask = LayerMask.GetMask("LayeredSolid", "Solid", "Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        points[0] = transform.position;
+
+        CalculateRays();
+        DrawRays();
+    }
+
+    void DrawRays()
+    {
+        for (int i=0; i< maxReflection + 1; i ++)
+        {
+            if( points[i] != null)
+            {
+                lineRenderer.SetPosition(i, points[i]);
+            }
+        }
+    }
+
+    void CalculateRays()
+    {
+        for (int i = 1; i < maxReflection + 2; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(points[i-1], transform.right, range, collisionMask);
+
+            if (hit.collider)
+                lineRenderer.SetPosition(i, hit.point);
+            else
+                lineRenderer.SetPosition(i, transform.position + (transform.right * range));
+        }
+    }
+}
