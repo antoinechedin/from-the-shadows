234c99c7fb5d654724aa4ebaefd1379a23a7ff9f
Max Emsallem <emsallem.max@hotmail.fr>
2020-02-25 11:52:26 +0100

    Added Projectile spawner and Projectile

---
+++ Assets/Scripts/Props/SkeletonBoss/ProjectileSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public float projectileCooldown = 5f;
+    public float projectileSpeed = 2f;
+
+    public GameObject projectile;
+
+    public GameObject target;
+
+    private GameObject[] players = new GameObject[2];
+
+    void Start()
+    {
+        for (int i = 0; i < GameObject.FindGameObjectsWithTag("Player").Length; i++)
+        {
+            players[i] = GameObject.FindGameObjectsWithTag("Player")[i];
+        }
+
+        StartCoroutine(SpawnProjectileLoop());
+    }
+
+    IEnumerator SpawnProjectileLoop()
+    {
+        yield return new WaitForSeconds(projectileCooldown);
+
+        Instantiate(projectile, this.transform.position, Quaternion.identity, this.transform);
+        StartCoroutine(SpawnProjectileLoop());
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(this.transform.position, players[0].transform.position) >= Vector3.Distance(this.transform.position, players[1].transform.position))
+            target = players[1];
+        else
+            target = players[0];
+    }
+}
---
+++ Assets/Scripts/Props/SkeletonBoss/SkeletonBossProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonBossProjectile : MonoBehaviour
+{
+    private float speed;
+    private Vector3 targetPosition;
+
+    private Vector3 direction;
+    void Start()
+    {
+        speed = this.transform.parent.GetComponent<ProjectileSpawner>().projectileSpeed;
+        targetPosition = this.transform.parent.GetComponent<ProjectileSpawner>().target.transform.position;
+
+        direction = (targetPosition - this.transform.position ).normalized;
+
+        StartCoroutine(DestroyGameObject());
+    }
+
+    IEnumerator DestroyGameObject()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(this.gameObject);
+    }
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
