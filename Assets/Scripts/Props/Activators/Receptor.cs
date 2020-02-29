d05b58ca0ee658427d885fff5daa599fafe9081d
GaelleMarais <marais.gaelle@gmail.com>
2020-02-29 12:24:58 +0100

    Update materials

--- Assets/Scripts/Props/Activators/Receptor.cs
+++ Assets/Scripts/Props/Activators/Receptor.cs
@@ -19,17 +19,6 @@ public class Receptor : Activator, IResetable
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("allo");
-    }
-
     public void On()
     {
         if (TryActivate != null && !active)
