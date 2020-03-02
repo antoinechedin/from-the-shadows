76cfc27cded9f6e7a66f889883e9203f1bd282b0
Max Emsallem <emsallem.max@hotmail.fr>
2020-03-02 11:35:42 +0100

    Added moving killzone to test

---
+++ Assets/Scenes/Game/Chapters/Max/ToBeDeleted/PreBossWall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreBossWall : MonoBehaviour, IResetable
+{
+    public bool isHorizontal = true;
+    public float speed = 2f;
+
+    private Vector3 basePosition;
+    private void Start()
+    {
+        basePosition = this.transform.position;
+    }
+    void Update()
+    {
+        if (isHorizontal)
+            this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        else
+            this.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+    }
+
+    public void Reset()
+    {
+        this.transform.position = basePosition;
+    }
+}
