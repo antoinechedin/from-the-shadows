7364e7dbd61549238bed6ca562ae61a294f5dff7
Rendy LATBI <r.latbi@laposte.net>
2020-03-02 14:22:47 +0100

    Things You Know

---
+++ Assets/Scripts/Controllers/Player/PlayerParticles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParticles : MonoBehaviour
+{
+    public ParticleSystem psLeft;
+    public ParticleSystem psRight;
+
+    public void PlayFootstepParticles(float right)
+    { 
+        if ((right == 0.1f || right == 1.1f || Random.Range(0, 2) == 0) && Time.time > 2)
+        {
+            if (right > 0.5) psRight.Play();
+            else psLeft.Play();
+        }
+    }
+}
