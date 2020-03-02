c9153caf3a5a9b399749593c2a25489792d895a0
Max Emsallem <emsallem.max@hotmail.fr>
2020-03-02 13:18:32 +0100

    Test with preboss level

--- Assets/Scenes/Game/Chapters/Max/ToBeDeleted/PreBossWall.cs
+++ Assets/Scenes/Game/Chapters/Max/ToBeDeleted/PreBossWall.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PreBossWall : MonoBehaviour, IResetable
 {
     public bool isHorizontal = true;
+    public bool isQuaranteCinqDegre = false;
     public float speed = 2f;
 
     private Vector3 basePosition;
@@ -14,7 +15,9 @@ public class PreBossWall : MonoBehaviour, IResetable
     }
     void Update()
     {
-        if (isHorizontal)
+        if(isQuaranteCinqDegre)
+            this.transform.position += new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, 0);
+        else if (isHorizontal && !isQuaranteCinqDegre)
             this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         else
             this.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
