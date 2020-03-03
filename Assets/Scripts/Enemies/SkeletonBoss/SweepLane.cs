39db91e9b5cf015c54efb971b4cb57b6209a83ea
GaelleMarais <marais.gaelle@gmail.com>
2020-03-03 10:51:45 +0100

    Spawn spikes

--- Assets/Scripts/Enemies/SkeletonBoss/SweepLane.cs
+++ Assets/Scripts/Enemies/SkeletonBoss/SweepLane.cs
@@ -4,39 +4,81 @@ using UnityEngine;
 
 public class SweepLane : MonoBehaviour
 {
-    public Transform left;
-    public Transform right;
+    public Transform[] points; 
     public GameObject prefab;
     public float speed;
+    public float minSecondsAfterRespawn = 3;
+    public float maxSecondsAfterRespawn = 6;
 
     private GameObject go;
+    private Vector3 from;
+    private Vector3 to;
+    private int randomLane = -1;
+    private bool forceChangeLane = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("Spawn");
+        Invoke("Spawn", 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (go != null)
-            go.transform.position = Vector3.MoveTowards(go.transform.position, right.position, Time.deltaTime * speed);
-        if (go != null && go.transform.position == right.position)
+            go.transform.position = Vector3.MoveTowards(go.transform.position, to, Time.deltaTime * speed);
+        if (go != null && go.transform.position == to)
         {
             go = null;
             Destroy(go);
+            float randomTime = Random.Range(minSecondsAfterRespawn, maxSecondsAfterRespawn);
+            Invoke("Spawn", randomTime);
         }
     }
 
-    public IEnumerator Spawn()
+    public void Spawn()
     {
-        for(int i = 0; i < 5; i++)
+        // Choose a random lane
+        int random = Random.Range(0, points.Length / 2);
+
+        // If the lane do not change, we only change the direction
+        // if it is the same lane for the 3rd time we re-execute the funtion until it changes
+        if( random == randomLane)
         {
-            if (left != null && right != null)
-                go = Instantiate(prefab, left.position, Quaternion.identity);
-            yield return new WaitForSeconds(5);
+            if (forceChangeLane)
+            {
+                Spawn();
+                return;
+            }
+            else
+            {
+                SwitchDirection();
+                forceChangeLane = true;
+            }
         }
-        yield return null;
+        else
+        {
+            // Else we set the new lane
+            forceChangeLane = false;
+            randomLane = random;
+            from = points[randomLane * 2].position;
+            to = points[randomLane * 2 + 1].position;
+
+            // and choose a random direction
+            int randomDirection = Random.Range(0, 2);
+            if (randomDirection == 1)
+                SwitchDirection();
+        }
+
+        // Spawn the prefab
+        if (from != null && to != null && prefab != null)
+            go = Instantiate(prefab, from, Quaternion.identity);
+    }
+
+    public void SwitchDirection()
+    {
+        Vector3 tmp = from;
+        from = to;
+        to = tmp;
     }
 }
