f1345acdbfc45260cde35675db56914066f217a2
antoinechedin <antoine.chedin@gmail.com>
2020-03-04 15:33:28 +0100

    Deprecate old falling platforms

--- Assets/Scripts/Props/FallingPlatform.cs
+++ Assets/Scripts/Props/FallingPlatform.cs
@@ -23,7 +23,7 @@ public class FallingPlatform : MonoBehaviour, IResetable
     // Start is called before the first frame update
     private void Start()
     {
-        if (transform.childCount < 2)
+        if (transform.childCount != 2)
         {
             Debug.LogWarning("WARN FallingPlatform.Start: " + Utils.GetFullName(transform)
                              + " is invalid, couldn't find child mesh/collider");
@@ -32,33 +32,37 @@ public class FallingPlatform : MonoBehaviour, IResetable
         {
             plaformCollider = transform.GetChild(0).GetComponent<Collider2D>();
             mesh = transform.GetChild(1);
-        }
-        if (plaformCollider == null)
-        {
-            Debug.LogWarning("WARN FallingPlatform.Start: "  + Utils.GetFullName(transform.GetChild(0))
-                             + " don't have 2D Collider");
-        }
+            if (plaformCollider == null)
+            {
+                Debug.LogWarning("WARN FallingPlatform.Start: " + Utils.GetFullName(transform.GetChild(0))
+                                 + " don't have 2D Collider");
+            }
 
-        startingPosition = transform.position;
-        startingColor = mesh.GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
-        targetColor = startingColor;
-        fallingPosition = transform.position;
-
+            startingPosition = transform.position;
+            if (mesh != null)
+            {
+                startingColor = mesh.GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
+                targetColor = startingColor;
+            }
+            fallingPosition = transform.position;
+        }
     }
 
     public void Update()
     {
-        if (isShaking)
+        if (mesh != null)
         {
-            mesh.position = new Vector3(mesh.position.x + Mathf.Sin(Time.time * shakeSpeed) * shakeIntensity,
-                                         mesh.position.y,
-                                         mesh.position.z);
+            if (isShaking)
+            {
+                mesh.position = new Vector3(mesh.position.x + Mathf.Sin(Time.time * shakeSpeed) * shakeIntensity,
+                                             mesh.position.y,
+                                             mesh.position.z);
+            }
+            Color color = mesh.GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
+            Color fade = Color.Lerp(color, targetColor, Time.deltaTime * 10);
+            mesh.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", fade);
+            mesh.position = Vector3.Lerp(mesh.position, fallingPosition, Time.deltaTime * 5);
         }
-
-        Color color = mesh.GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
-        Color fade = Color.Lerp(color, targetColor, Time.deltaTime * 10);
-        mesh.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", fade);
-        mesh.position = Vector3.Lerp(mesh.position, fallingPosition, Time.deltaTime * 5);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -75,16 +79,19 @@ public class FallingPlatform : MonoBehaviour, IResetable
         if (plaformCollider != null)
             plaformCollider.enabled = false;
         targetColor = new Color(startingColor.r, startingColor.g, startingColor.b, 0);
-        fallingPosition = mesh.position - new Vector3(0, 5, 0);
+        if (mesh != null)
+            fallingPosition = mesh.position - new Vector3(0, 5, 0);
         Invoke("Reset", timerBeforeSpawning);
     }
 
     public void Reset()
     {
         isShaking = false;
-        mesh.position = startingPosition;
+        if (mesh != null)
+            mesh.position = startingPosition;
         fallingPosition = startingPosition;
-        plaformCollider.enabled = true;
+        if (plaformCollider != null)
+            plaformCollider.enabled = true;
         targetColor = startingColor;
         CancelInvoke();
     }
