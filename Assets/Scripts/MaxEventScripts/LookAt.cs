b7d77d7426e23d7cd31002bfc519dac18f1f185d
Max Emsallem <emsallem.max@hotmail.fr>
2020-03-06 14:12:22 +0100

    Theatre

---
+++ Assets/Scripts/MaxEventScripts/LookAt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        this.transform.LookAt(target);
+    }
+}
