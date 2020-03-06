e0c4085a59cfe8c4ac199cdfaad38d121540eeae
Leomani <xboxlive84320@hotmail.fr>
2020-03-06 15:51:53 +0100

    Parenter la MovingKillZone au level dans lequel elle est pour éviter le despaw

--- Assets/Scripts/Enemies/MovingKillZoneCP.cs
+++ Assets/Scripts/Enemies/MovingKillZoneCP.cs
@@ -9,11 +9,13 @@ public class MovingKillZoneCP : MonoBehaviour
     public float timeToMaxSpeed;
 
     private MovingKillZone movingKillZone;
+    private GameObject currentLevel;
 
     // Start is called before the first frame update
     void Start()
     {
-        movingKillZone = GetComponentInParent<MovingKillZone>();
+        movingKillZone = GameObject.FindObjectOfType<MovingKillZone>();
+        currentLevel = GetComponentInParent<LevelManager>().gameObject;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,6 +23,7 @@ public class MovingKillZoneCP : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             movingKillZone.SetNewInfos(this);
+            movingKillZone.transform.SetParent(currentLevel.transform);
         }
     }
 }
