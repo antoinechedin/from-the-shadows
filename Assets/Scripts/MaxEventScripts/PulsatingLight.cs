2bae9b006aa06e40a78c17623183b81d38448711
Max Emsallem <emsallem.max@hotmail.Fr>
2020-03-07 13:47:15 +0100

    Added lights to some levels

---
+++ Assets/Scripts/MaxEventScripts/PulsatingLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsatingLight : MonoBehaviour
+{
+    public int startingIntensity = 12000;
+    public int endingIntensity = 6000;
+
+    public float speed = 2f;
+
+    public bool playAtStart = false;
+
+    private bool mustPulse = false;
+
+    private Light light;
+    private float t = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = this.GetComponent<Light>();
+        if (playAtStart)
+            mustPulse = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustPulse)
+        {
+            t += speed * Time.deltaTime;
+
+            light.intensity = Mathf.Lerp(startingIntensity, endingIntensity, t);
+        }
+    }
+}
