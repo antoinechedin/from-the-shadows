5b6601d3802e32ff9bb2390e34fa3770ffb4bb09
Leomani <xboxlive84320@hotmail.fr>
2020-03-11 16:10:50 +0100

    Début de script prism

---
+++ Assets/Scripts/Enemies/Boss Chap 2/BossPressurePlate.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BossPressurePlate : Activator
+{
+    public AudioClip sound;
+    public Material activeMat;
+    public Material inactiveMat;
+    public string tagInteractObject;
+
+    private GameObject child;
+    private AudioSource audioSource;
+    private int nbObjectsOnPlate;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        child = transform.Find("Child").gameObject;
+        Off();
+    }
+
+    /// <summary>
+    /// Activate when an Object is on the Plate or a Player
+    /// </summary>
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag(tagInteractObject))
+        {
+            updateNbObjectsOnPlate(+1);
+        }
+    }
+
+    /// <summary>
+    /// Deactivate when the Object or the Player leaves the Plate
+    /// </summary>
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag(tagInteractObject))
+        {
+            updateNbObjectsOnPlate(-1);
+        }
+    }
+
+    /// <summary>
+    /// Activate the pressure plate
+    /// </summary>
+    protected void On()
+    {
+        if (TryActivate != null)
+        {
+            if (audioSource != null)
+                audioSource.PlayOneShot(sound);
+            if (child != null)
+                child.GetComponent<MeshRenderer>().material = activeMat;
+
+        }
+    }
+
+    /// <summary>
+    /// Deactivate the pressure plate 
+    /// </summary>
+    protected void Off()
+    {
+        if (TryDeactivate != null)
+        {
+            if (child != null)
+                child.GetComponent<MeshRenderer>().material = inactiveMat;
+        }
+    }
+
+    void updateNbObjectsOnPlate(int i)
+    {
+        nbObjectsOnPlate += i;
+
+        if (nbObjectsOnPlate == 1)
+            On();
+        if (nbObjectsOnPlate == 0)
+        {
+            Off();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Boss Chap 2/Prism.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prism : Receptor
+{
+    public int nbLaserToActivate;
+
+    private int currentNbLaser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public new void On()
+    {
+        Debug.Log("++");
+        currentNbLaser++;
+
+        if (currentNbLaser == nbLaserToActivate)
+        {
+            //faire la fonction qui tire un rayon
+        }
+    }
+
+    public new void Off()
+    {
+        Debug.Log("--");
+        currentNbLaser--;
+    }
+
+}
