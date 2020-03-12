f1177d05a1825d2d03ef169c2d299547a0016562
Romain Roy <rroy.romain@gmail.com>
2020-03-12 11:34:38 +0100

    Désactives les particules quand les torches sont désactivées

--- Assets/Scripts/Props/Listeners/Torch.cs
+++ Assets/Scripts/Props/Listeners/Torch.cs
@@ -17,12 +17,24 @@ public class Torch : ActivatorListener, IResetable
     public bool active;
     private AudioSource audioSource;
     private float targetRadius = 0.01f;
+    private GameObject model;
+    private GameObject flamme;
+    private ParticleSystem particles;
+    private bool isTorch = false; // et pas un chandelier
 
     private void Awake()
     {
         lightSource = transform.Find("LightSource").gameObject;
         lightSource.GetComponent<NewLightSource>().lightRadius = 0f;
         pointLight = lightSource.transform.Find("PointLight").gameObject;
+
+        if (transform.Find("Model"))
+        {
+            isTorch = true;
+            model = transform.Find("Model").gameObject;
+            flamme = model.transform.Find("Flamme").gameObject;
+            particles = flamme.GetComponent<ParticleSystem>();
+        }
     }
 
     void Start()
@@ -33,7 +45,7 @@ public class Torch : ActivatorListener, IResetable
         {
             lightSource.GetComponent<NewLightSource>().lightRadius = 0f;
             OnActivate();
-        }     
+        }
 
         isMute = false;
         active = activeAtStart;
@@ -44,7 +56,7 @@ public class Torch : ActivatorListener, IResetable
         if (Mathf.Abs(targetRadius - lightSource.GetComponent<NewLightSource>().lightRadius) < 0.001f && !neverStatic)
             lightSource.GetComponent<NewLightSource>().GoStatic();
 
-        lightSource.GetComponent<NewLightSource>().lightRadius = Mathf.Lerp(lightSource.GetComponent<NewLightSource>().lightRadius, targetRadius, Time.deltaTime*10);
+        lightSource.GetComponent<NewLightSource>().lightRadius = Mathf.Lerp(lightSource.GetComponent<NewLightSource>().lightRadius, targetRadius, Time.deltaTime * 10);
         pointLight.GetComponent<Light>().range = lightSource.GetComponent<NewLightSource>().lightRadius;
     }
 
@@ -52,7 +64,7 @@ public class Torch : ActivatorListener, IResetable
     {
         lightSource.GetComponent<LightCollider>().SetStatic(false);
         targetRadius = lightRadius;
-
+        if (isTorch) particles.Play();
         active = true;
         if (audioSource != null && !isMute)
             audioSource.PlayOneShot(soundOn);
@@ -61,7 +73,7 @@ public class Torch : ActivatorListener, IResetable
     public override void OnDeactivate()
     {
         lightSource.GetComponent<LightCollider>().SetStatic(false);
-
+        if (isTorch) particles.Stop();
         targetRadius = 0.01f;
         active = false;
         if (audioSource != null && !isMute)
