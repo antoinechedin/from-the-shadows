d4c7a6c34c5dad11887ed990c142ceb0463204f2
Aloïs <marrocq.alois@laposte.net>
2020-03-17 14:37:50 +0100

    Script to control Outline

---
+++ Assets/Scripts/Utils/Outline.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline : MonoBehaviour
+{
+    public Material mat;
+    public MeshRenderer mesh;
+    public int materialIndex;
+    public Color outlineColor;
+
+    private Material instanceMat;
+
+    private void Awake()
+    {
+        List<Material> list = new List<Material>();
+        mesh.GetMaterials(list);
+        instanceMat = list[materialIndex];
+        instanceMat.SetColor("_Color", outlineColor);
+        HideOutline();
+    }
+
+    public void DisplayOutline()
+    {
+        instanceMat.SetFloat("_Alpha", 1);
+    }
+
+    public void HideOutline()
+    {
+        instanceMat.SetFloat("_Alpha", 0);
+    }
+}
