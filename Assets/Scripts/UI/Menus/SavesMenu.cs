21a32e20d27caade8fea2dc56e761824c92da322
antoinechedin <antoine.chedin@gmail.com>
2020-03-18 11:37:12 +0100

    Fix ActionChoice for SavesMenu and add Carousel border

--- Assets/Scripts/UI/Menus/SavesMenu.cs
+++ Assets/Scripts/UI/Menus/SavesMenu.cs
@@ -10,22 +10,20 @@ using TMPro;
 public class SavesMenu : MonoBehaviour
 {
     public MenuManager menuManager;
+    public RectTransform actionChoiceButtons;
+    public RectTransform newGameChoiceButtons;
+
     public enum State { Loading, Saving };
     public State currentState;
     public Button[] savesButons;
     private Save[] saves;
 
-    private RectTransform actionChoicePanel;
-    private RectTransform newGameChoicePanel;
     private int lastSelected = 0;
 
     void Start()
     {
         saves = GameManager.Instance.Saves;
         UpdateButtons();
-        actionChoicePanel = gameObject.transform.Find("SavesActionsButtons").gameObject.GetComponent<RectTransform>();
-        // newGameChoicePanel = gameObject.transform.Find("New Game Choice").gameObject.GetComponent<RectTransform>();
-        menuManager = GameObject.Find("Menu Manager").gameObject.GetComponent<MenuManager>();
         if (savesButons.Length > 0)
         {
             EventSystem.current.SetSelectedGameObject(savesButons[0].gameObject);
@@ -36,14 +34,14 @@ public class SavesMenu : MonoBehaviour
     {
         if (Input.GetButtonDown("B_G"))
         {
-            if (actionChoicePanel != null && actionChoicePanel.gameObject.activeSelf)
+            if (actionChoiceButtons != null && actionChoiceButtons.gameObject.activeSelf)
             {
-                actionChoicePanel.gameObject.SetActive(false);
+                actionChoiceButtons.gameObject.SetActive(false);
                 EventSystem.current.SetSelectedGameObject(savesButons[lastSelected].gameObject);
             }
-            else if (newGameChoicePanel != null && newGameChoicePanel.gameObject.activeSelf)
+            else if (newGameChoiceButtons != null && newGameChoiceButtons.gameObject.activeSelf)
             {
-                newGameChoicePanel.gameObject.SetActive(false);
+                newGameChoiceButtons.gameObject.SetActive(false);
                 EventSystem.current.SetSelectedGameObject(savesButons[lastSelected].gameObject);
             }
             else
@@ -71,10 +69,10 @@ public class SavesMenu : MonoBehaviour
                     return;
                 }
 
-                string saveName = "Save " + i;
-                string completion = "NULL" + " %";
-                string timePlayed = TimeSpan.FromSeconds(GameManager.Instance.GetMetaFloat("totalTimePlayed", i)).ToString(@"hh\:mm\:ss");
-                
+                string saveName = "Save " + (i + 1).ToString();
+                string completion = "00" + "%";
+                string timePlayed = TimeSpan.FromSeconds(GameManager.Instance.GetMetaFloat("totalTimePlayed", i)).ToString(@"hh\:mm");
+
                 buttonText.text = saveName + "   " + completion + " " + timePlayed;
             }
             else
@@ -110,37 +108,30 @@ public class SavesMenu : MonoBehaviour
     {
         lastSelected = index;
 
-        Button playButton = actionChoicePanel.transform.Find("Play Button").GetComponent<Button>();
-        Button deleteButton = actionChoicePanel.transform.Find("Delete Button").GetComponent<Button>();
-        Button soloButton = newGameChoicePanel.transform.Find("Solo Button").GetComponent<Button>();
-        Button duoButton = newGameChoicePanel.transform.Find("Duo Button").GetComponent<Button>();
-
-        playButton.onClick.RemoveAllListeners();
-        deleteButton.onClick.RemoveAllListeners();
-        soloButton.onClick.RemoveAllListeners();
-        duoButton.onClick.RemoveAllListeners();
-
         if (currentState == State.Loading && saves[index] != null)
         {
-            actionChoicePanel = gameObject.transform.Find("Action Choice").gameObject.GetComponent<RectTransform>();
+            Button playButton = actionChoiceButtons.transform.Find("PlayButton").GetComponent<Button>();
+            Button deleteButton = actionChoiceButtons.transform.Find("DeleteButton").GetComponent<Button>();
 
-
-            actionChoicePanel.gameObject.SetActive(true);
+            actionChoiceButtons.gameObject.SetActive(true);
             EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(playButton.gameObject);
-
+            
+            playButton.onClick.RemoveAllListeners();
             playButton.onClick.AddListener(delegate { Launch(index); });
             deleteButton.onClick.RemoveAllListeners();
             deleteButton.onClick.AddListener(delegate { Delete(index); });
         }
         else if (saves[index] == null)
         {
-            newGameChoicePanel = gameObject.transform.Find("New Game Choice").gameObject.GetComponent<RectTransform>();
+            Button soloButton = newGameChoiceButtons.transform.Find("SoloButton").GetComponent<Button>();
+            Button duoButton = newGameChoiceButtons.transform.Find("DuoButton").GetComponent<Button>();
 
+            newGameChoiceButtons.gameObject.SetActive(true);
+            EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(soloButton.gameObject);
 
-            newGameChoicePanel.gameObject.SetActive(true);
-            EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(duoButton.gameObject);
-
+            soloButton.onClick.RemoveAllListeners();
             soloButton.onClick.AddListener(delegate { NewGame(1, index); });
+            duoButton.onClick.RemoveAllListeners();
             duoButton.onClick.AddListener(delegate { NewGame(2, index); });
         }
     }
@@ -152,8 +143,8 @@ public class SavesMenu : MonoBehaviour
     public void Launch(int indexSave)
     {
         GameManager.Instance.CurrentSave = indexSave;
-        actionChoicePanel.gameObject.SetActive(false);
-        newGameChoicePanel.gameObject.SetActive(false);
+        actionChoiceButtons.gameObject.SetActive(false);
+        newGameChoiceButtons.gameObject.SetActive(false);
         List<Chapter> chapters = GameManager.Instance.GetChapters();
         GameManager.Instance.CurrentChapter = 0;
         for (int i = 0; i < chapters.Count - 1; i++)
@@ -175,7 +166,7 @@ public class SavesMenu : MonoBehaviour
         SaveManager.Instance.DeleteSaveFile(indexSave);
         Debug.Log("Delete save number " + indexSave);
 
-        gameObject.transform.Find("Action Choice").gameObject.SetActive(false);
+        actionChoiceButtons.gameObject.SetActive(false);
         EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(savesButons[indexSave].gameObject);
 
         saves[indexSave] = null;
