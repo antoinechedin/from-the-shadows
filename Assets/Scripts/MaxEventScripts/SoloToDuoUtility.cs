5a627b7482844b7d7f982aeff32a78ad0b9d7ac0
Max Emsallem <emsallem.max@hotmail.fr>
2020-03-19 13:25:27 +0100

    Edited camera target for when in solo mode

--- Assets/Scripts/MaxEventScripts/SoloToDuoUtility.cs
+++ Assets/Scripts/MaxEventScripts/SoloToDuoUtility.cs
@@ -7,10 +7,18 @@ public class SoloToDuoUtility : MonoBehaviour
 {
     public void SwitchToSolo()
     {
-        Debug.Log("solo");
+        Debug.Log("Switched to Solo Mode");
 
         // TODO : Change Player1 & Player2 to PlayerSolo
-        // Change Camera for Solo
+        // Change dialogues ? Or maybe just enable/disable new one in the Solo/Duo level prefab
+        GameObject.Find("Players").transform.GetChild(0).gameObject.SetActive(true); // Enable SoloPlayers
+        GameObject.Find("Players").transform.GetChild(1).gameObject.SetActive(false); // Disable DuoPlayers
+
+
+        CameraTarget cameraTarget = FindObjectOfType<CameraTarget>();
+        cameraTarget.isSolo = true; // Set the Camera Target to follow only one player
+        EditorUtility.SetDirty(cameraTarget);
+
 
         foreach (LevelManager level in FindObjectsOfType<LevelManager>()) // Enable the solo version and disable the duo version
         {
@@ -21,16 +29,28 @@ public class SoloToDuoUtility : MonoBehaviour
             }
         }
 
+
         foreach (ChangeLevelTrigger levelTrigger in FindObjectsOfType<ChangeLevelTrigger>()) // Set required player to change level to 1
         {
             levelTrigger.nbNecessaryPlayers = 1;
             EditorUtility.SetDirty(levelTrigger);
         }
+
     }
 
     public void SwitchToDuo()
     {
-        Debug.Log("duo");
+        Debug.Log("Switched to Duo Mode");
+
+
+        GameObject.Find("Players").transform.GetChild(0).gameObject.SetActive(false); // Enable SoloPlayers
+        GameObject.Find("Players").transform.GetChild(1).gameObject.SetActive(true); // Disable DuoPlayers
+
+
+        CameraTarget cameraTarget = FindObjectOfType<CameraTarget>();
+        cameraTarget.isSolo = false; // Set the Camera Target to follow only one player
+        EditorUtility.SetDirty(cameraTarget);
+
 
         foreach (LevelManager level in FindObjectsOfType<LevelManager>())
         {
@@ -40,6 +60,7 @@ public class SoloToDuoUtility : MonoBehaviour
                 level.duoLevel.SetActive(true);
             }
         }
+
 
         foreach (ChangeLevelTrigger levelTrigger in FindObjectsOfType<ChangeLevelTrigger>())
         {
