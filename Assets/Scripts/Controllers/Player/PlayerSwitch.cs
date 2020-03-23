b16655b0581fbe1183740be5cbc3c0db2acedfc1
Aloïs <marrocq.alois@laposte.net>
2020-03-23 11:55:05 +0100

    Can toggle, Solo can't attack but is able to

---
+++ Assets/Scripts/Controllers/Player/PlayerSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerSwitch : MonoBehaviour
+{
+    public enum CurrentPlayer { Light, Shadow };
+
+    public Material shadowMat;
+    public Material lightMat;
+
+    public SkinnedMeshRenderer mesh;
+
+    private CurrentPlayer currentPlayer;
+
+    private void Awake()
+    {
+        currentPlayer = CurrentPlayer.Shadow;
+        mesh.material = shadowMat;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TogglePlayer();
+        }
+    }
+
+    private void TogglePlayer()
+    {
+        if(currentPlayer == CurrentPlayer.Shadow)
+        {
+            currentPlayer = CurrentPlayer.Light;
+            mesh.material = lightMat;
+        }
+        else
+        {
+            currentPlayer = CurrentPlayer.Shadow;
+            mesh.material = shadowMat;
+        }
+    }
+}
