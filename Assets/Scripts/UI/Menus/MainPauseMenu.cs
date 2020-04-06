67effe9309c115466e7bec6d8388d98810002035
antoinechedin <antoine.chedin@gmail.com>
2020-04-06 15:58:39 +0200

    Update pause menu

---
+++ Assets/Scripts/UI/Menus/MainPauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainPauseMenu : MonoBehaviour, IDissolveMenu
+{
+    public Selectable resumeButton;
+    public Selectable optionsButton;
+
+    public IEnumerator DissolveInCoroutine()
+    {
+        gameObject.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(optionsButton.gameObject);
+
+        DissolveController[] dissolves = GetComponentsInChildren<DissolveController>();
+        for (int i = 0; i < dissolves.Length - 1; i++)
+        {
+            StartCoroutine(dissolves[i].DissolveInCoroutine(MenuManager.dissolveDuration));
+            yield return new WaitForSecondsRealtime(MenuManager.dissolveOffset);
+        }
+
+        EventSystem.current.sendNavigationEvents = true;
+
+        yield return StartCoroutine(dissolves[dissolves.Length - 1].DissolveInCoroutine(MenuManager.dissolveDuration));
+    }
+
+    public IEnumerator DissolveOutCoroutine()
+    {
+        EventSystem.current.sendNavigationEvents = false;
+
+        DissolveController[] dissolves = GetComponentsInChildren<DissolveController>();
+        for (int i = 0; i < dissolves.Length - 1; i++)
+        {
+            StartCoroutine(dissolves[i].DissolveOutCoroutine(MenuManager.dissolveDuration));
+            yield return new WaitForSecondsRealtime(MenuManager.dissolveOffset);
+        }
+
+        yield return StartCoroutine(dissolves[dissolves.Length - 1].DissolveOutCoroutine(MenuManager.dissolveDuration));
+
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/UI/Menus/PauseMenu.cs
+++ Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -9,17 +9,36 @@ using UnityEditor;
 
 public class PauseMenu : MonoBehaviour
 {
-    public Button resumeButton;
+    public MainPauseMenu mainPauseMenu;
+    public OptionsMenu optionsMenu;
+
+    private bool optionsOpened;
+
+    private void Awake()
+    {
+        optionsOpened = false;
+        gameObject.SetActive(false);
+    }
 
     public void OpenPauseMenu()
     {
         Time.timeScale = 0;
-        EventSystem.current.SetSelectedGameObject(resumeButton.gameObject);
+        EventSystem.current.SetSelectedGameObject(mainPauseMenu.resumeButton.gameObject);
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Start_G")) Resume();
+        if (EventSystem.current.sendNavigationEvents)
+        {
+            if (optionsOpened)
+            {
+                if (Input.GetButtonDown("Start_G") || Input.GetButtonDown("B_G")) CloseOptions();
+            }
+            else
+            {
+                if (Input.GetButtonDown("Start_G")) Resume();
+            }
+        }
     }
 
     public void Resume()
@@ -39,10 +58,28 @@ public class PauseMenu : MonoBehaviour
         GameManager.Instance.LoadMenu("MainMenu", new LoadingMenuInfo(2));
     }
 
-    public void Options()
+    public void OpenOptions()
     {
-        // Time.timeScale = 1;
-        // TODO: Menu Options
+        StartCoroutine(OpenOptionsCoroutine());
+    }
+
+    private IEnumerator OpenOptionsCoroutine()
+    {
+        yield return StartCoroutine(mainPauseMenu.DissolveOutCoroutine());
+        optionsOpened = true;
+        yield return StartCoroutine(optionsMenu.DissolveInCoroutine());
+    }
+
+    public void CloseOptions()
+    {
+        StartCoroutine(CloseOptionsCoroutine());
+    }
+
+    private IEnumerator CloseOptionsCoroutine()
+    {
+        yield return StartCoroutine(optionsMenu.DissolveOutCoroutine());
+        optionsOpened = false;
+        yield return StartCoroutine(mainPauseMenu.DissolveInCoroutine());
     }
 
     public void Quit()
