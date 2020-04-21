1b441467e29efda118947b00512f76a22640a5ad
Max Emsallem <emsallem.max@hotmail.fr>
2020-04-21 14:39:23 +0200

    Fixed Neutral Platform prefab & working on boss cinematic

---
+++ Assets/Scripts/MaxEventScripts/CameraShake.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float amplitude = 1f;
+    public float frequency = 1f;
+
+    public void ShakeFor(float time)
+    {
+        StartCoroutine(ShakeForAsync(amplitude, frequency, time));
+    }
+
+    public IEnumerator ShakeForAsync(float amplitude, float frequency, float time)
+    {
+        StartCameraShake(amplitude, frequency);
+        yield return new WaitForSeconds(time);
+        StopCameraShake();
+    }
+
+    public void StartCameraShake(float amplitude, float frequency)
+    {
+        this.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
+        this.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
+    }
+
+    public void StopCameraShake()
+    {
+        this.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0f;
+    }
+
+}
